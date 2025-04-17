b718d7774ca327cbc69d081317a3dc4a7ca4fd98
Romain Ragonnet <romain.ragonnet@monash.edu>
2025-04-17 10:57:03 +1000

    Use prop_early among activators rather than prop_early among all infections

--- model.py
+++ model.py
@@ -225,18 +225,18 @@ def get_stratified_latency_flow_rates(studies:list, all_cause_mortality_func:Cal
     for study in studies:
         # early progression rate
         stratified_latency_flow_rates["activation_rate_early"][study] = (
-            Parameter("prop_early_activators") / Parameter("mean_duration_early_latent")
+            Parameter("prop_early_among_activators") * Parameter("lifelong_activation_risk") / Parameter("mean_duration_early_latent")
         )
 
         # late progression rate
         stratified_latency_flow_rates["stabilisation_rate"][study] = (
-            (1. - Parameter("prop_early_activators")) / Parameter("mean_duration_early_latent")  - all_cause_mortality_func 
+            (1. - Parameter("prop_early_among_activators") * Parameter("lifelong_activation_risk")) / Parameter("mean_duration_early_latent")  - all_cause_mortality_func 
         )
 
         # stabilisation rate
         stratified_latency_flow_rates["activation_rate_late"][study] = (
-            all_cause_mortality_func * (
-                Parameter("lifelong_activation_risk") - Parameter("prop_early_activators")
+            all_cause_mortality_func * Parameter("lifelong_activation_risk") * (
+                1. - Parameter("prop_early_among_activators")
                 ) / (
                     1. - all_cause_mortality_func * Parameter("mean_duration_early_latent") - Parameter("lifelong_activation_risk")
                     )
