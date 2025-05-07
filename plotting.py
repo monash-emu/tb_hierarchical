108cd84d84bbbcaa1ec5cd39e4142dcce200b095
Romain Ragonnet <romain.ragonnet@monash.edu>
2025-05-07 13:20:16 +1000

    Create plotting module

---
+++ plotting.py
@@ -0,0 +1,113 @@
+from pathlib import Path
+import arviz as az
+from copy import copy
+import matplotlib.pyplot as plt
+import numpy as np
+
+from estival import priors as esp
+
+
+def plot_traces(idata, burn_in, output_folder=None):
+    az.rcParams["plot.max_subplots"] = 60 # to make sure all parameters are included in trace plots
+
+    if output_folder:
+        output_folder_path = Path(output_folder) / "mc_outputs"
+        output_folder_path.mkdir(exist_ok=True, parents=True)
+
+    chain_length = idata.sample_stats.sizes['draw']
+
+    # burn data
+    burnt_idata = idata.sel(draw=range(burn_in, chain_length))  # Discard burn-in
+
+    # Traces (after burn-in)
+    az.plot_trace(burnt_idata, figsize=(16, 3.0 * len(idata.posterior)), compact=False);
+    plt.subplots_adjust(hspace=.7)
+    if output_folder:
+        plt.savefig(output_folder_path / "mc_traces.jpg", facecolor="white", bbox_inches='tight')
+        plt.close()
+
+
+def plot_post_prior_comparison(
+    idata: az.InferenceData,
+    req_vars: list,
+    priors: list,
+    req_grid=None,
+    req_size=None,
+) -> plt.figure:
+    """Plot comparison of calibration posterior estimates
+    for parameters against their prior distributions.
+
+    Args:
+        idata: Calibration inference data
+        req_vars: Names of the parameters to plot
+        priors: Prior distributions for the parameters
+        req_grid: Dimensions of the subplot
+        req_size: Figure size request
+
+    Returns:
+        The figure
+    """
+    grid = req_grid if req_grid else [1, len(req_vars)]
+    size = req_size if req_size else None
+    fig = az.plot_density(idata, var_names=req_vars, shade=0.3, grid=grid, figsize=size, hdi_prob=1.)
+    for i_ax, ax in enumerate(fig.ravel()):
+        ax_limits = ax.get_xlim()
+        param = ax.title.get_text().split("\n")[0]
+        if param:
+            x_vals = np.linspace(*ax_limits, 50)
+            distri = priors[i_ax]
+
+            if type(distri) != esp.TruncNormalPrior:
+                y_vals = np.exp(distri.logpdf(x_vals))
+                
+                ax.fill_between(x_vals, y_vals, color="k", alpha=0.2, linewidth=2)
+    # ax.figure.suptitle(country, fontsize=30, y=1.0)
+    return ax.figure.tight_layout()
+
+
+def plot_model_fit_with_uncertainty(axis, uncertainty_df, output_name, bcm, include_legend=True, x_min=None):
+
+    # update_rcparams() 
+   
+    df = uncertainty_df[output_name]
+
+    if output_name in bcm.targets:
+        t = copy(bcm.targets[output_name].data)
+        axis.scatter(list(t.index), t, marker=".", color='black', label='observations', zorder=11, s=5.)
+
+    colour = (0.2, 0.2, 0.8)   
+
+    time = df.index
+    axis.plot(time, df[0.5], color=colour, zorder=10, label="model (median)")
+
+    axis.fill_between(
+        time, 
+        df[0.25], df[0.75], 
+        color=colour, 
+        alpha=0.5, 
+        edgecolor=None,
+        label="model (IQR)"
+    )
+    axis.fill_between(
+        time, 
+        df[0.025], df[0.975],
+        color=colour, 
+        alpha=0.3,
+        edgecolor=None,
+        label="model (95% CI)",
+    )
+
+    if output_name == "transformed_random_process":
+        axis.set_ylim((0., axis.get_ylim()[1]))
+    
+    if x_min:
+        axis.set_xlim((x_min, axis.get_xlim()[1]))
+
+    # axis.tick_params(axis="x", labelrotation=45)
+    title = output_name # if output_name not in title_lookup else title_lookup[output_name]
+
+    axis.set_ylabel(title)
+    # plt.tight_layout()
+
+    if include_legend:
+        plt.legend(markerscale=2.)
