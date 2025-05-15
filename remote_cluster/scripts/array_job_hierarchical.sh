1e0fb9c85a76ef015a1c9808777df56731ef5c3a
Romain Ragonnet <romain.ragonnet@monash.edu>
2025-05-15 13:37:47 +1000

    Fix output log path issue

--- remote_cluster/scripts/array_job_hierarchical.sh
+++ remote_cluster/scripts/array_job_hierarchical.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=4096
 #SBATCH --cpus-per-task=4
-#SBATCH --output=/sh30/users/rragonnet/tb_hierarchical/remote_cluster/scripts/logs/%j.out 
+#SBATCH --output=/home/rrag0004/sh30/users/rragonnet/tb_hierarchical/remote_cluster/logs/%j.out 
 
 # To receive an email when job completes or fails
 #SBATCH --mail-user=romain.ragonnet@monash.edu
