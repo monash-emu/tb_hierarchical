85a6d474791621b8f55829dfbad9bc9aa676fce8
Romain Ragonnet <romain.ragonnet@monash.edu>
2025-05-15 13:09:49 +1000

    Redirect slurm outputs

--- remote_cluster/scripts/array_job_hierarchical.sh
+++ remote_cluster/scripts/array_job_hierarchical.sh
@@ -5,6 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=4096
 #SBATCH --cpus-per-task=4
+#SBATCH --output=/sh30/users/rragonnet/tb_hierarchical/remote_cluster/scripts/logs/%j.out 
 
 # To receive an email when job completes or fails
 #SBATCH --mail-user=romain.ragonnet@monash.edu
