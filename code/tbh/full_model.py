62ba8b8172261d1d8116ace067f98a34c0f95718
Romain Ragonnet <romain.ragonnet@monash.edu>
2025-07-16 11:37:56 +1000

    Include excess births and create population output

--- code/tbh/full_model.py
+++ code/tbh/full_model.py
@@ -2,6 +2,8 @@ from summer2 import CompartmentalModel, Stratification
 from summer2.parameters import Parameter, DerivedOutput
 from summer2.functions import time as stf
 
+from tbh.outputs import request_model_outputs
+
 from jax import numpy as jnp
 import yaml
 from pathlib import Path
@@ -9,7 +11,23 @@ from typing import Callable
 
 HOME_PATH = Path(__file__).parent.parent.parent
 
-PLACEHOLDER_VALUE = 1.
+PLACEHOLDER_VALUE = 0.
+
+COMPARTMENTS = (
+    "mtb_naive",
+    "incipient",
+    "contained",
+    "cleared",
+    "subclin_noninf",
+    "clin_noninf",
+    "subclin_inf",
+    "clin_inf",
+    "treatment",
+    "recovered"
+)
+
+INFECTIOUS_COMPARTMENTS = ("subclin_inf", "clin_inf")
+
 
 def get_tb_model(model_config: dict, home_path=HOME_PATH):
     """
@@ -22,32 +40,17 @@ def get_tb_model(model_config: dict, home_path=HOME_PATH):
 
     # stratify by age
 
-    # request_model_outputs(model)
+    request_model_outputs(model, COMPARTMENTS)
 
     return model
 
 
 def get_natural_tb_model(model_config):
 
-    compartments = (
-        "mtb_naive",
-        "incipient",
-        "contained",
-        "cleared",
-        "subclin_noninf",
-        "clin_noninf",
-        "subclin_inf",
-        "clin_inf",
-        "treatment",
-        "recovered"
-    )
-
-    infectious_compartments = ["subclin_inf", "clin_inf"]
-
     model = CompartmentalModel(
         times=(model_config["start_time"], model_config["end_time"]),
-        compartments=compartments,
-        infectious_compartments=infectious_compartments,
+        compartments=COMPARTMENTS,
+        infectious_compartments=INFECTIOUS_COMPARTMENTS,
     )
 
     model.set_initial_population(
@@ -60,13 +63,21 @@ def get_natural_tb_model(model_config):
     # Natural mortality modelled as transition back to mtb_naive compartment
     #FIXME! will need to make sure all goes to children after age-stratification
     #FIXME!: will need to add flow from mtb_naiveXadult to mtb_naiveXchild after age-stratification
-    for compartment in [c for c in compartments if c != "mtb_naive"]:
+    for compartment in [c for c in COMPARTMENTS if c != "mtb_naive"]:
         model.add_transition_flow(
             name=f"all_cause_mortality_from_{compartment}",
             fractional_rate=PLACEHOLDER_VALUE,  #FIXME! placeholder
             source=compartment,
             dest="mtb_naive",
         )
+
+    # Excess birth to capture population growth
+    #FIXME! will need to make sure all goes to children after age-stratification
+    model.add_crude_birth_flow(
+        name="excess_birth",
+        birth_rate=PLACEHOLDER_VALUE, #FIXME! placeholder
+        dest="mtb_naive"
+    )
 
 
     # Early TB infection flows
---
+++ code/tbh/outputs.py
@@ -0,0 +1,18 @@
+
+from summer2 import CompartmentalModel
+from summer2.parameters import Parameter, DerivedOutput
+
+
+def request_model_outputs(model: CompartmentalModel, compartments: list):
+    """
+    Define model outputs that can later be requested from model.get_derived_outputs_df()
+
+    Args:
+        model (CompartmentalModel): the 'fully-built' TB model
+        compartments (list): list of all model compartments (required to create population size output)
+        studies (list): list of studies (required to disaggregate outputs by study)
+    """
+    ## Outputs relative to population size
+    model.request_output_for_compartments(
+        name=f"population", compartments=compartments
+    )
