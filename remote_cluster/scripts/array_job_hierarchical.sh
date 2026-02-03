7254d9447bc6d812a2f86780ab65e9ba5e8f6ab7
Romain Ragonnet <romain.ragonnet@monash.edu>
2026-02-03 14:17:56 +1100

    Increase walltime for massive

--- remote_cluster/scripts/array_job_hierarchical.sh
+++ remote_cluster/scripts/array_job_hierarchical.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=heterogenous_mixing
+#SBATCH --job-name=dynamic_mixing
 #SBATCH --account=sh30
-#SBATCH --time=12:00:00
+#SBATCH --time=15:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=8G
 #SBATCH --cpus-per-task=4
